b7947422a77fa1d9a7011057bb67f6d5ff0c4b6e
sullom101 <mayow.sullom@gmail.com>
2021-01-11 17:44:35 +0800

    add sectionHeading component

---
+++ src/components/SectionHeading/SectionHeading.js
@@ -0,0 +1,43 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styles from "./section-heading.module.scss";
+import Button from "../Button/Button";
+const SectionHeading = ({
+  buttonTitle,
+  title,
+  subTitle,
+  titleStyle,
+  subTitleStyle,
+  buttonStyle,
+  className
+}) => {
+  return (
+    <div className={`${styles.titleWrapper} ${className}`}>
+      <h2 className={`${styles.title} ${titleStyle}`}>{title}</h2>
+      <p className={`${styles.subTitle} ${subTitleStyle}`}>{subTitle}</p>
+      <Button className={`${styles.buttonStyle} ${buttonStyle}`}>{buttonTitle}</Button>
+    </div>
+  );
+};
+
+SectionHeading.propTypes = {
+  title: PropTypes.string,
+  subTitle: PropTypes.string,
+  buttonTitle: PropTypes.string,
+  titleStyle: PropTypes.string,
+  subTitleStyle: PropTypes.string,
+  buttonStyle: PropTypes.string,
+  className: PropTypes.string
+};
+
+SectionHeading.defaultProps = {
+  title: "Games Hub",
+  subTitle: "The best offers, new games, AAA titles and high-quality video games..",
+  buttonTitle: "Discover All",
+  titleStyle: "",
+  subTitleStyle: "",
+  buttonStyle: "",
+  className: ""
+};
+
+export default SectionHeading;
