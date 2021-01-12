2fe2f27c6655876ececd25b7cb3a4f3b6d2e5b66
sullom101 <mayow.sullom@gmail.com>
2021-01-12 18:04:06 +0800

    add slider component

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,15 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import styles from "./slider.module.scss";
+import { SliderData } from "./SliderData";
+import Button from "../Button/Button";
 
-const Slider = (props) => {
+const Slider = ({ slider }) => {
+  const [current, setCurrent] = useState(0);
+  const length = slider.length;
+
+  const nextSlide = () => {
+    setCurrent(current === length - 1 ? 0 : current + 1);
+  };
+
+  const prevSlide = () => {
+    setCurrent(current === 0 ? length - 1 : current - 1);
+  };
+
+  if (!Array.isArray(slider) || slider.length <= 0) {
+    return null;
+  }
+
   return (
     <section className={styles.sectionWrapper}>
-      <div></div>
+      <Button className={styles.goLeft} onClick={prevSlide}>
+        PREV
+      </Button>
+      <Button className={styles.goRight} onClick={nextSlide}>
+        NEXT
+      </Button>
+      {SliderData.map((item, index) => {
+        return (
+          <React.Fragment key={index}>
+            <div className={index === current ? styles.slideLeft : styles.slide}>
+              {index === current && <img src={item.image} alt="" className={styles.imageLeft} />}
+            </div>
+            <div className={index === current ? styles.slideActive : styles.slide}>
+              {index === current && <img src={item.image} alt="" className={styles.imageActive} />}
+            </div>
+            <div className={index === current ? styles.slideRight : styles.slide}>
+              {index === current && <img src={item.image} alt="" className={styles.imageRight} />}
+            </div>
+          </React.Fragment>
+        );
+      })}
+      <div className={styles.dotWrapper}>
+        {slider.map((dot, index) => {
+          return (
+            <Button
+              key={`dot-${index}`}
+              className={current === index ? styles.dotActive : styles.dot}
+              onClick={() => setCurrent(index)}></Button>
+          );
+        })}
+      </div>
     </section>
   );
 };
 
-Slider.propTypes = {};
+Slider.propTypes = {
+  slider: PropTypes.arrayOf([PropTypes.shape({ title: PropTypes.string, image: PropTypes.string })])
+};
+Slider.defaultProps = {
+  slider: SliderData
+};
 
 export default Slider;
---
+++ src/components/Slider/SliderData.js
@@ -0,0 +1,22 @@
+export const SliderData = [
+  {
+    title: "Become a legendary Viking warrior raised on tales of battle and glory.",
+    image: "/images/slider.png"
+  },
+  {
+    title: "Become a legendary Viking warrior raised on tales of battle and glory.",
+    image: "/images/slider.png"
+  },
+  {
+    title: "Become a legendary Viking warrior raised on tales of battle and glory.",
+    image: "/images/slider.png"
+  },
+  {
+    title: "Become a legendary Viking warrior raised on tales of battle and glory.",
+    image: "/images/slider.png"
+  },
+  {
+    title: "Become a legendary Viking warrior raised on tales of battle and glory.",
+    image: "/images/slider.png"
+  }
+];
