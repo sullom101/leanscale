80bd4976f251b07574bfc5d8c6331474a71d44c1
sullom101 <mayow.sullom@gmail.com>
2021-01-12 18:03:22 +0800

    update header componetn

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import styles from './header.module.scss';
+import React from "react";
+import PropTypes from "prop-types";
+import styles from "./header.module.scss";
+import Search from "../Search/Search";
 
 const Header = (props) => {
   return (
     <header className={styles.headerWrapper}>
       <div className={styles.navigationWrapper}>
-        <div className={styles.logoWrapper}>
-          {/* <h1>
-                     sullom
-                 </h1> */}
-        </div>
-        <div className={styles.search}>
-          <label className={styles.label}>
-            <input
-              type="text"
-              className={styles.inputItem}
-              placeholder="Search games, gears, accessories.."
-            />
-          </label>
-          <span className={styles.searchIcon}></span>
-        </div>
+        <div className={styles.logoWrapper}></div>
+        <Search
+          searchWrapper={styles.search}
+          labelStyle={styles.label}
+          inputStyle={styles.inputItem}
+          placeholder="Search games, gears, accessories.."
+          iconStyle={styles.searchIcon}
+        />
         <div className={styles.iconListWrapper}>
           <div className={styles.iconListItems}>
             <div className={styles.iconListitem}>
