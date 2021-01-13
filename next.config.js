ac655c0ea65649ae5b628fa497c79893d9ba0b8f
sullom101 <mayow.sullom@gmail.com>
2021-01-13 21:55:06 +0800

    next config for env and using constants in the project

---
+++ next.config.js
@@ -0,0 +1,29 @@
+const { parsed: localEnv } = require("dotenv").config();
+const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
+
+module.exports = (phase, { defaultConfig }) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+      ...defaultConfig,
+      target: "server",
+      compress: true,
+      cssModules: true,
+      generateEtags: true,
+      env: {
+        appUrl: localEnv.APP_URL,
+        apiUrl: localEnv.API_URL
+      }
+    };
+  }
+  return {
+    ...defaultConfig,
+    target: "serverless",
+    compress: true,
+    cssModules: true,
+    generateEtags: true,
+    env: {
+      appUrl: localEnv.APP_URL,
+      apiUrl: localEnv.API_URL
+    }
+  };
+};
